70d79a7175429c133623ffe09c2eda5ec2ea9f4d
must <mustafa.tahir@etraveli.com>
2014-05-06 21:30:34 +0200

    reformat code

--- Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlPagesConverterTest.java
+++ Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlPagesConverterTest.java
@@ -1,16 +1,15 @@
 package tddmicroexercises.textconvertor;
 
-import static org.junit.Assert.*;
-
 import java.io.IOException;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 
 public class HtmlPagesConverterTest {
-    @Test
-    public void foo() throws IOException {
-        HtmlPagesConverter converter = new HtmlPagesConverter("foo");
-        assertEquals("fixme", converter.getFilename());
-    }
+  @Test
+  public void foo() throws IOException {
+    HtmlPagesConverter converter = new HtmlPagesConverter("foo");
+    assertEquals("fixme", converter.getFilename());
+  }
 }
--- Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
+++ Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
@@ -1,13 +1,13 @@
 package tddmicroexercises.textconvertor;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class HtmlTextConverterTest {
-    @Test
-    public void foo() {
-        HtmlTextConverter converter = new HtmlTextConverter("foo");
-        assertEquals("fixme", converter.getFilename());
-    }
+  @Test
+  public void foo() {
+    HtmlTextConverter converter = new HtmlTextConverter("foo");
+    assertEquals("fixme", converter.getFilename());
+  }
 }
